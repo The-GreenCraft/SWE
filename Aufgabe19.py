f555268d24d08b6b33b8abeb59a6f73d70a31629
The-GreenCraft <th3greencraft@gmail.com>
2024-04-09 12:28:13 +0200

    modified:   Aufgabe19.py

--- Aufgabe19.py
+++ Aufgabe19.py
@@ -1,12 +1,16 @@
 #Aufgabe 19
 import csv
+
+year = 24 * 365
+print(year)
+
 with open("Day-ahead Prices_202201010000-202301010000.csv") as csvfile:
     reader = csv.reader(csvfile, delimiter=" ")
-    for row in reader:
-        print(f", ".join(row)) 
+    # for row in reader:
+    #     print(f", ".join(row)) 
 
 with open("Total Load - Day Ahead _ Actual_202201010000-202301010000.csv") as csvfile:
     reader = csv.reader(csvfile, delimiter=" ")
-    for row in reader:
-        print(f", ".join(row))    
+    # for row in reader:
+    #     print(f", ".join(row))    
 
