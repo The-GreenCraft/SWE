46fc0f356007f3a3ac19c164f2b69870bf945e1f
The-GreenCraft <th3greencraft@gmail.com>
2024-04-09 12:09:37 +0200

    modified:   Aufgabe19.py

--- Aufgabe19.py
+++ Aufgabe19.py
@@ -1,1 +1,6 @@
 #Aufgabe 19
+import csv
+with open("Day-ahead Prices_202201010000-202301010000.csv") as csvfile:
+    reader = csv.reader(csvfile, delimiter=" ")
+    for row in reader:
+        print(f", ".join(row)) 
